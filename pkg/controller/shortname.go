d3e927db21c43db45ee96e6b985fc92cea15f638
Adam Harrison <harrisonadamw@gmail.com>
2016-09-07 14:35:34 +0100

    WIP

--- pkg/controller/shortname.go
+++ pkg/controller/shortname.go
@@ -5,24 +5,26 @@ import (
 	"math/big"
 )
 
-// sha1 hash an arbitrary string and represent it using the full range of
-// printable ascii characters
-func shortName(arbitrary string) string {
+const (
 	// This array:
 	// * Must include only printable UTF8 characters that are represented with a single byte
 	// * Must be at least of length 85 (`len("weave-") + l(2^160)/l(85)` equals 31, the maximum ipset name length)
 	// * Must not include commas as those are treated specially by `ipset add` when adding a named set to a list:set
 	// * Should not include space for readability
 	// * Should not include single quote or backslash to be nice to shell users
-	symbols := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789(){}[]<>_$%^&*|/?.;:@#~")
+	ShortNameSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789(){}[]<>_$%^&*|/?.;:@#~"
+)
+
+// sha1 hash an arbitrary string and represent it using the full range of
+// printable ascii characters
+func shortName(arbitrary string) string {
+	symbols := []byte(ShortNameSymbols)
 	sum := sha1.Sum([]byte(arbitrary))
 	i := big.NewInt(0).SetBytes(sum[:])
 	base := big.NewInt(int64(len(symbols)))
 	zero := big.NewInt(0)
-
 	result := make([]byte, 0)
 
-	// TODO pad to generate constant length strings
 	for i.Cmp(zero) > 0 {
 		remainder := new(big.Int).Mod(i, base)
 		i.Sub(i, remainder)
